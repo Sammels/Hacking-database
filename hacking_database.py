e5077ea08eb4a9ae3205db6e67f87b530adc9492
Sammels <Sasagete@gmail.com>
2023-08-08 14:02:12 +0300

    Создал декоратора с исключениями

--- hacking_database.py
+++ hacking_database.py
@@ -6,48 +6,49 @@ from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
+def exception_decorator(func):
+    """Декоратор принимает в себя функцию, и оборачивает её исключениями"""
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ObjectDoesNotExist:
+            print("Такого имени не существует")
+        except MultipleObjectsReturned:
+            print("Найдено несколько учеников")
+    return wrapper
+
+
+@exception_decorator
 def fix_marks(schoolkid: str):
     """Функция изменяет оценки."""
-    try:
-        user_name = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
-        user_name = Mark.objects.filter(schoolkid__full_name__contains=user_name, points__lte=3)
-        ids = []
-        for point in user_name:
-            ids.append(point.id)
+    user_name = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
+    user_name = Mark.objects.filter(schoolkid__full_name__contains=user_name, points__lte=3)
+    ids = []
+    for point in user_name:
+        ids.append(point.id)
 
-        for point in ids:
-            work_process = Mark.objects.get(id=point)
-            work_process.points = 5
-            work_process.save()
-
-        print("Работа окончена. You are HACKERMAN...")
-
-    except ObjectDoesNotExist:
-        print("Такого имени не существует")
-    except MultipleObjectsReturned:
-        print("Найдено несколько учеников")
+    for point in ids:
+        work_process = Mark.objects.get(id=point)
+        work_process.points = 5.
+        work_process.save()
+    print("Работа окончена. You are HACKERMAN...")
 
 
+@exception_decorator
 def remove_chastisements(schoolkid: str):
     """Функция удаляет замечания ученику"""
-    try:
-        user_name = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
-        user_name = Chastisement.objects.filter(schoolkid__full_name__contains=f"{schoolkid}")
-        ids = []
-        for point in user_name:
-            ids.append(point.id)
+    user_name = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
+    user_name = Chastisement.objects.filter(schoolkid__full_name__contains=f"{schoolkid}")
+    ids = []
+    for point in user_name:
+        ids.append(point.id)
 
-        for point in ids:
-            work_process = Chastisement.objects.get(id=point)
-            work_process.delete()
+    for point in ids:
+        work_process = Chastisement.objects.get(id=point)
+        work_process.delete()
 
-        print("Работа окончена. You are BAD...")
-
-    except ObjectDoesNotExist:
-        print("Такого имени не существует")
-    except MultipleObjectsReturned:
-        print("Найдено несколько учеников")
-
+    print("Работа окончена. You are BAD...")
 
 
 def create_commendation(schoolkid: str, subject: str) -> str:
@@ -64,31 +65,25 @@ def create_commendation(schoolkid: str, subject: str) -> str:
                      "Ты многое сделал, я это вижу!", "Теперь у тебя точно все получится!", ]
     choice_commendation = choice(commendations)
 
-    try:
-        schoolkid_check = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
-        schoolkid_year = schoolkid_check.year_of_study
-        schoolkid_group_letter = schoolkid_check.group_letter
-        schoolkid_id = schoolkid_check.id
+    schoolkid_check = Schoolkid.objects.get(full_name__contains=f"{schoolkid}")
+    schoolkid_year = schoolkid_check.year_of_study
+    schoolkid_group_letter = schoolkid_check.group_letter
+    schoolkid_id = schoolkid_check.id
 
-        subject_ = Subject.objects.filter(title__contains=f"{subject}",
-                                          year_of_study__contains=schoolkid_year)
-        subject_id = subject_[0].id
+    subject_ = Subject.objects.filter(title__contains=f"{subject}",
+                                      year_of_study__contains=schoolkid_year)
+    subject_id = subject_[0].id
 
-        teacher_identification = Lesson.objects.filter(subject_id=subject_id, group_letter=schoolkid_group_letter,
-                                                       year_of_study=schoolkid_year)
-        teacher_id = teacher_identification[0].teacher_id
-        lesson_date = teacher_identification[0].date
+    teacher_identification = Lesson.objects.filter(subject_id=subject_id, group_letter=schoolkid_group_letter,
+                                                   year_of_study=schoolkid_year)
+    teacher_id = teacher_identification[0].teacher_id
+    lesson_date = teacher_identification[0].date
 
-        # Create inst
-        Commendation.objects.create(teacher_id=teacher_id, subject_id=subject_id, schoolkid_id=schoolkid_id,
-                                    text=choice_commendation, created=lesson_date)
+    # Create inst
+    Commendation.objects.create(teacher_id=teacher_id, subject_id=subject_id, schoolkid_id=schoolkid_id,
+                                text=choice_commendation, created=lesson_date)
 
-        print("Ты очень ПЛОХОЙ.")
-
-    except ObjectDoesNotExist:
-        print("Такого имени, или предмета не существует")
-    except MultipleObjectsReturned:
-        print("Найдено несколько учеников")
+    print("Ты очень ПЛОХОЙ.")
 
 
 print("Knock-knock. KGB open up!!!!!")
