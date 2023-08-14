0bc92bc564b540ad40f5536f77cd56ff3334bf79
Sammels <Sasagete@gmail.com>
2023-08-14 14:17:41 +0300

    Удалил лишнии запросы

--- hacking_database.py
+++ hacking_database.py
@@ -63,11 +63,13 @@ def create_commendation(schoolkid: str, subject: str) -> str:
                                       year_of_study__contains=schoolkid_year).first()
     subject_id = subject_.id
 
-    lesson_identification = Lesson.objects.all().order_by('subject_id', 'group_letter', 'year_of_study').values().first()
+    lesson_identification = Lesson.objects.order_by('subject_id', 'group_letter', 'year_of_study').first()
+
+    print(lesson_identification)
 
 
-    teacher_id = lesson_identification['teacher_id']
-    lesson_date = lesson_identification['date']
+    teacher_id = lesson_identification.teacher_id
+    lesson_date = lesson_identification.date
 
     Commendation.objects.create(teacher_id=teacher_id, subject_id=subject_id, schoolkid_id=schoolkid_id,
                                 text=choice_commendation, created=lesson_date)
